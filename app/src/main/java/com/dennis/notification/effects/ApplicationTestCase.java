432dcb634fe50692a0a856318a43e44d51ed2851
DELL <muasyadennis4@gmail.com>
2020-10-21 14:49:04 +0300

    commit

---
+++ app/src/main/java/com/dennis/notification/effects/ApplicationTestCase.java
@@ -0,0 +1,9 @@
+package com.dennis.notification.effects;
+
+import android.app.Application;
+
+class ApplicationTestCase<T> {
+    public ApplicationTestCase(Class<Application> applicationClass) {
+
+    }
+}
